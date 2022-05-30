01c0c0bf5e02cb706da2d08e9f906fbdc2ede03d
missile <missilexuan@gmail.com>
2022-05-30 13:50:19 +0800

    实现便利文件夹导入

--- src/views/player/component/Avatar.vue
+++ src/views/player/component/Avatar.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import a575700182 from '@/assets/avatar/575700182.gif'
-import a540042242 from '@/assets/avatar/540042242.gif'
-import a558241106 from '@/assets/avatar/558241106.png'
-import a2022515210 from '@/assets/avatar/2022515210.jpg'
-import a557740389 from '@/assets/avatar/557740389.gif'
 
 defineProps<{accountId: number}>()
-const imgMap = ref<{[key:number]:string}>({
-  575700182: a575700182,
-  540042242: a540042242,
-  558241106: a558241106,
-  2022515210: a2022515210,
-  557740389: a557740389,
-  558145708: a557740389
-})
+
+const imgs = import.meta.glob('@/assets/avatar/*')
+const imgMap = ref<{[key:number]:string}>({})
+
+for (const path in imgs) {
+  imgs[path]().then(img => {
+    console.log(img.default)
+    const pathArry = (img.default as string).split('/')
+    imgMap.value[parseInt(pathArry[pathArry.length - 1].split('.')[0])] = img.default
+  })
+}
+
 </script>
 <template>
   <div v-if="imgMap[accountId]" style="padding-right: 20px;">
