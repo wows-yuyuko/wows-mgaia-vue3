ac4b9fa104658505c5b47c0e2bf559c64abfb2dc
missile <missilexuan@gmail.com>
2022-06-06 09:38:13 +0800

    增加rank数据显示

--- src/views/player/Player.vue
+++ src/views/player/Player.vue
@@ -12,6 +12,7 @@ import moment from 'moment'
 import * as echarts from 'echarts'
 import n404 from '@/assets/404.png'
 import { ElMessage } from 'element-plus'
+import { buildBattlesEchart, buildRecentBattlesEchart } from './hooks/buildChart'
 
 const player = usePlayer()
 
@@ -167,188 +168,29 @@ const getUserShip = (playerItem: Player) => {
         classifyShip[ship.shipInfo.shipInfo.level][ship.shipInfo.shipInfo.shipType].battles += ship.shipInfo.battles
         classifyShip[ship.shipInfo.shipInfo.level][ship.shipInfo.shipInfo.shipType].wins += ship.shipInfo.wins
       }
-      buildEchart(classifyShip)
+      // 构建战斗图表
+      buildBattlesEchart(classifyShip, battlesEchart)
+      nextTick(() => {
+        echartsResize()
+      })
     })
 }
 
-// 构建战斗图表
-const buildEchart = (classifyShip: any) => {
-  const option:any = {
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'shadow' // 阴影指示器
-      }
-    },
-    legend: {
-      textStyle: {
-        color: 'white'
-      },
-      data: ['DD', 'CA', 'BB', 'CV']
-    },
-    title: {
-      text: '战斗数',
-      textStyle: {
-        color: 'white',
-        fontSize: 24
-      }
-    },
-    yAxis: [{
-      type: 'value',
-      axisLabel: {
-        fontSize: 16,
-        color: 'white'
-      }
-    }],
-    xAxis: [
-      {
-        type: 'category',
-        data: ['Ⅰ', 'Ⅱ', 'Ⅲ', 'Ⅳ', 'Ⅴ', 'Ⅵ', 'Ⅶ', 'Ⅷ', 'Ⅸ', 'X', 'Xl'],
-        axisLabel: {
-          color: 'white'
-        }
-      }
-    ],
-    series: []
-  }
-  option.series.push(
-    {
-      name: 'DD',
-      type: 'bar',
-      stack: 'battles',
-      data: [
-        classifyShip[1]?.Destroyer?.battles,
-        classifyShip[2]?.Destroyer?.battles,
-        classifyShip[3]?.Destroyer?.battles,
-        classifyShip[4]?.Destroyer?.battles,
-        classifyShip[5]?.Destroyer?.battles,
-        classifyShip[6]?.Destroyer?.battles,
-        classifyShip[7]?.Destroyer?.battles,
-        classifyShip[8]?.Destroyer?.battles,
-        classifyShip[9]?.Destroyer?.battles,
-        classifyShip[10]?.Destroyer?.battles,
-        classifyShip[11]?.Destroyer?.battles
-      ]
-      // label: {
-      //   show: true
-      // }
-    }
-  )
-  option.series.push(
-    {
-      name: 'CA',
-      type: 'bar',
-      stack: 'battles',
-      data: [
-        classifyShip[1]?.Cruiser?.battles,
-        classifyShip[2]?.Cruiser?.battles,
-        classifyShip[3]?.Cruiser?.battles,
-        classifyShip[4]?.Cruiser?.battles,
-        classifyShip[5]?.Cruiser?.battles,
-        classifyShip[6]?.Cruiser?.battles,
-        classifyShip[7]?.Cruiser?.battles,
-        classifyShip[8]?.Cruiser?.battles,
-        classifyShip[9]?.Cruiser?.battles,
-        classifyShip[10]?.Cruiser?.battles,
-        classifyShip[11]?.Cruiser?.battles
-      ]
-    }
-  )
-  option.series.push(
-    {
-      name: 'BB',
-      type: 'bar',
-      stack: 'battles',
-      data: [
-        classifyShip[1]?.Battleship?.battles,
-        classifyShip[2]?.Battleship?.battles,
-        classifyShip[3]?.Battleship?.battles,
-        classifyShip[4]?.Battleship?.battles,
-        classifyShip[5]?.Battleship?.battles,
-        classifyShip[6]?.Battleship?.battles,
-        classifyShip[7]?.Battleship?.battles,
-        classifyShip[8]?.Battleship?.battles,
-        classifyShip[9]?.Battleship?.battles,
-        classifyShip[10]?.Battleship?.battles,
-        classifyShip[11]?.Battleship?.battles
-      ]
-    }
-  )
-  option.series.push(
-    {
-      name: 'CV',
-      type: 'bar',
-      stack: 'battles',
-      data: [
-        classifyShip[1]?.AirCarrier?.battles,
-        classifyShip[2]?.AirCarrier?.battles,
-        classifyShip[3]?.AirCarrier?.battles,
-        classifyShip[4]?.AirCarrier?.battles,
-        classifyShip[5]?.AirCarrier?.battles,
-        classifyShip[6]?.AirCarrier?.battles,
-        classifyShip[7]?.AirCarrier?.battles,
-        classifyShip[8]?.AirCarrier?.battles,
-        classifyShip[9]?.AirCarrier?.battles,
-        classifyShip[10]?.AirCarrier?.battles,
-        classifyShip[11]?.AirCarrier?.battles
-      ]
-    }
-  )
-  const count = []
-  function null0 (value: any) {
-    if (lodash.isNumber(value)) {
-      return value
-    } else {
-      return 0
-    }
-  }
-  for (let i = 0; i < 11; i++) {
-    count[i] = null0(classifyShip[i + 1]?.Destroyer?.battles) +
-      null0(classifyShip[i + 1]?.Cruiser?.battles) +
-      null0(classifyShip[i + 1]?.Battleship?.battles) +
-      null0(classifyShip[i + 1]?.AirCarrier?.battles)
-  }
-  option.series.push(
-    {
-      name: '总计',
-      type: 'bar',
-      barGap: '-100%', // 左移100%，stack不再与上面两个在一列,
-      data: count,
-      label: {
-        normal: {
-          show: true, // 显示数值
-          position: 'top', //  位置设为top
-          formatter: '{c}',
-          textStyle: { color: 'white' } // 设置数值颜色
-        }
-      },
-      itemStyle: {
-        normal: {
-          color: 'rgba(128, 128, 128, 0)' // 设置背景颜色为透明
-        }
-      }
-    }
-  )
-  battlesEchart.setOption(option)
-  nextTick(() => {
-    echartsResize()
-  })
-}
 // 刷新图表操作
 const echartsResize = () => {
   recentBattlesEchart?.resize()
   battlesEchart?.resize()
 }
+// 页面变动时刷新图表
+window.addEventListener('resize', () => {
+  echartsResize()
+})
 // 获取玩家近期数据
 const recentDate = ref<any>({})
 const recentDay = ref('') // 当前选中的日期
 const recentSevenDate = ref<any>({}) // 近七天数据统计
 const recentBattlesDiv = ref<HTMLElement|null>(null)
 let recentBattlesEchart!: echarts.ECharts
-// 页面变动时刷新图表
-window.addEventListener('resize', () => {
-  echartsResize()
-})
 const getUserRecent = async (playerItem: Player) => {
   // 获取近期数据
   accountRecentListV2({ ...playerItem }).then(
@@ -380,7 +222,7 @@ const getUserRecent = async (playerItem: Player) => {
           winColor: getWinColor(dayData.pvpInfo.wins),
           date: dateKey
         })
-        // 单船数据收集
+        // 近期每天数据整理
         recentDate.value[dateKey] = dayData
         // 船列表默认显示有数据的最后一个
         if (dayData.pvpInfo.battles > 0) {
@@ -398,79 +240,7 @@ const getUserRecent = async (playerItem: Player) => {
       }
       // 配置近期战斗图表
       recentBattlesEchart = echarts.init(recentBattlesDiv.value as HTMLElement)
-      const recentBattlesOption = {
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'shadow' // 阴影指示器
-          },
-          formatter: (params: any) => {
-            let text = '<div style="display: flex;justify-content:space-between;"><div>' + params[0].name + '</div></div>'
-            if (recentDate.value[params[0].name].pvpInfo.battles > 0) {
-              text += `<div style="display: flex;justify-content:space-between;"><div>PR： </div><div style="color:${recentDate.value[params[0].name].pvpInfo.pr.color}">${recentDate.value[params[0].name].pvpInfo.pr.value}</div></div>`
-              text += `<div style="display: flex;justify-content:space-between;"><div>胜率： </div><div style="color:${getWinColor(recentDate.value[params[0].name].pvpInfo.wins)}">${recentDate.value[params[0].name].pvpInfo.wins}%</div></div>`
-            }
-            text += `<div style="display: flex;justify-content:space-between;"><div>战斗数： </div><div>${params[0].value}</div></div>`
-            if (recentDate.value[params[0].name].pvpSoloInfo.battles > 0) {
-              text += `<div style="display: flex;justify-content:space-between;"><div>单野： </div><div>${recentDate.value[params[0].name].pvpSoloInfo.battles}</div></div>`
-            }
-            if (recentDate.value[params[0].name].pvpTwoInfo.battles > 0) {
-              text += `<div style="display: flex;justify-content:space-between;"><div>自行车： </div><div>${recentDate.value[params[0].name].pvpTwoInfo.battles}</div></div>`
-            }
-            if (recentDate.value[params[0].name].pvpThreeInfo.battles > 0) {
-              text += `<div style="display: flex;justify-content:space-between;"><div>装甲车： </div><div>${recentDate.value[params[0].name].pvpThreeInfo.battles}</div></div>`
-            }
-            return text
-          }
-        },
-        title: {
-          text: '近期战斗数（点击柱状图切换每日详情）',
-          textStyle: {
-            color: 'white',
-            fontSize: 24
-          }
-        },
-        yAxis: [{
-          type: 'value',
-          axisLabel: {
-            fontSize: 16,
-            color: 'white'
-          }
-        }],
-        xAxis: [
-          {
-            type: 'category',
-            data: battlesEchartData.map((value:any) => value.date),
-            axisLabel: {
-              color: 'white'
-            }
-          }
-        ],
-        series: [
-          {
-            name: '战斗数',
-            data: battlesEchartData.map((value:any) => {
-              return {
-                value: value.battles,
-                itemStyle: {
-                  color: getWinColor(value.wins)
-                }
-              }
-            }),
-            type: 'bar',
-            barMaxWidth: '10%',
-            label: {
-              normal: {
-                show: true, // 显示数值
-                position: 'top', //  位置设为top
-                formatter: '{c}',
-                textStyle: { color: 'white' } // 设置数值颜色
-              }
-            }
-          }
-        ]
-      }
-      recentBattlesEchart.setOption(recentBattlesOption)
+      buildRecentBattlesEchart(battlesEchartData, recentDate, recentBattlesEchart)
       recentBattlesEchart.on('click', (params) => {
         // 控制台打印数据的名称
         recentDay.value = params.name
@@ -836,19 +606,36 @@ const copyCommand = (text:string) => {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr v-for="ship of recentDate[recentDay]?.shipData" :key="ship.shipInfo.shipInfo.nameEnglish">
-                      <td class="icon"><img style="width:150px" :src="ship.shipInfo.shipInfo.imgSmall" /></td>
-                      <td class="name">{{ ship.shipInfo.shipInfo.nameCn }}</td>
-                      <td class="value">{{ ship.shipInfo.battles }}</td>
-                      <td class="value">
-                        <span :style="{color: ship.shipInfo.pr?.color}">
-                          {{ ship.shipInfo?.pr?.value }} {{ ship.shipInfo?.pr?.name }}
-                        </span>
-                      </td>
-                      <td class="value" :style="{color: getWinColor(ship.shipInfo.wins)}">{{ ship.shipInfo.wins }}%</td>
-                      <td class="value" :style="{color: getDamageColor(ship.shipInfo.shipInfo.shipType, ship.shipInfo.damage)}">{{ ship.shipInfo.damage }}</td>
-                      <td class="value">{{ ship.shipInfo.hit }}</td>
-                    </tr>
+                    <template v-for="ship of recentDate[recentDay]?.shipData" :key="ship.shipInfo.shipInfo.nameEnglish">
+                      <tr v-if="ship.shipInfo.battles > 0">
+                        <td class="icon"><img style="width:150px" :src="ship.shipInfo.shipInfo.imgSmall" /></td>
+                        <td class="name">{{ ship.shipInfo.shipInfo.nameCn }}</td>
+                        <td class="value">{{ ship.shipInfo.battles }}</td>
+                        <td class="value">
+                          <span :style="{color: ship.shipInfo.pr?.color}">
+                            {{ ship.shipInfo?.pr?.value }} {{ ship.shipInfo?.pr?.name }}
+                          </span>
+                        </td>
+                        <td class="value" :style="{color: getWinColor(ship.shipInfo.wins)}">{{ ship.shipInfo.wins }}%</td>
+                        <td class="value" :style="{color: getDamageColor(ship.shipInfo.shipInfo.shipType, ship.shipInfo.damage)}">{{ ship.shipInfo.damage }}</td>
+                        <td class="value">{{ ship.shipInfo.hit }}</td>
+                      </tr>
+                    </template>
+                    <template v-for="ship of recentDate[recentDay]?.shipData" :key="ship.rankSolo.shipInfo.nameEnglish">
+                      <tr v-if="ship.rankSolo.battles > 0">
+                        <td class="icon"><img style="width:150px" :src="ship.rankSolo.shipInfo.imgSmall" /></td>
+                        <td class="name">{{ ship.rankSolo.shipInfo.nameCn }}(rank)</td>
+                        <td class="value">{{ ship.rankSolo.battles }}</td>
+                        <td class="value">
+                          <span :style="{color: ship.rankSolo.pr?.color}">
+                            {{ ship.rankSolo?.pr?.value }} {{ ship.rankSolo?.pr?.name }}
+                          </span>
+                        </td>
+                        <td class="value" :style="{color: getWinColor(ship.rankSolo.wins)}">{{ ship.rankSolo.wins }}%</td>
+                        <td class="value" :style="{color: getDamageColor(ship.rankSolo.shipInfo.shipType, ship.rankSolo.damage)}">{{ ship.rankSolo.damage }}</td>
+                        <td class="value">{{ ship.rankSolo.hit }}</td>
+                      </tr>
+                    </template>
                   </tbody>
                 </table>
               </div>
