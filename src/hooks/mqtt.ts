08db0356e114f01cc37fa5341f94d53f5a34e077
missile <missilexuan@gmail.com>
2022-08-19 13:34:38 +0800

    mqtt测试

--- src/hooks/mqtt.ts
+++ src/hooks/mqtt.ts
@@ -5,25 +5,22 @@ const options = {
   clean: true, // 设置为 false 以在离线时接收 QoS 1 和 2 消息；
   connectTimeout: 4000,
   // Auth
-  clientId: 'mqttx_6b476a1e'
+  clientId: 'mgaia_web_test'
   // username: 'wows-poll',
   // password: 'wows-poll'
 }
 // const client = mqtt.connect('ws://mq.wows.linxun.link:8083/mqtt', options)
 const client = mqtt.connect('ws://broker.emqx.io:8083/mqtt', options)
 // 链接成功会触发connect
-client.on('connect', function () {
-  console.log('Connected')
-  client.subscribe('wows/test/poll', { qos: 1 }, function (err, res) {
-    console.log('触发', res)
-    if (!err) {
-      client.publish('wows/test/poll', 'Hello mqtt', { qos: 1, retain: true })
-    }
+client.on('connect', () => {
+  console.log('Connection succeeded!')
+  client.subscribe('wowstestpoll', { qos: 1 }, function (err, res) {
+    console.log('触发', res, err)
   })
 })
-
-client.on('message', function (topic:any, message:any) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
+client.on('error', error => {
+  console.log('Connection failed', error)
+})
+client.on('message', (topic, message) => {
+  console.log(`Received message ${message} from topic ${topic}`)
 })
